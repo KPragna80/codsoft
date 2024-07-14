23a75e78eec7f07929625d04e0494ba9d8e43a54
KPragna80 <pragnapriyakattta@gmail.com>
2024-07-14 16:34:36 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -5,8 +5,7 @@ lower="abcdefghijklmnopqrstuvwxyz"
 numbers="0123456789"
 symbols="{}?@$!%^*+_()"
 all=lower+upper+numbers+symbols
-lenth=16
-
+lenth=5
 for i in range(5):
     password="".join(random.sample(all,lenth))
     print(password)
