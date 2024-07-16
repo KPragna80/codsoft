9ca69630e6db8fdf8c754e35a16905bad5d71439
KPragna80 <pragnapriyakattta@gmail.com>
2024-07-16 15:59:58 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,20 @@
+first = input("enter 1st number:")
+second = input("enter 2nd number:")
+operator = input("enter an operator ( * - + % / ) :") 
+
+first = int(first)
+second = int(second)
+
+if operator == "*":
+    print(first * second)
+elif operator == "-":
+    print(first - second)
+elif operator == "+":
+    print(first + second)        
+elif operator == "%":
+    print(first % second)
+elif operator == "/":
+    print(first / second)
+else:
+    print("please enter valid input")
+
